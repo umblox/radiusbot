81b9268e363062cdf6b1eb855db8ff2bca1d3bfa
umblox <39423646+umblox@users.noreply.github.com>
2024-09-17 08:14:53 +0700

    Create index.php
    if you put this radiusbot to webserver folder

---
+++ radiusbot/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*******************************************************************************************************************
+* Warning!!!, Tidak untuk diperjual belikan!, Cukup pakai sendiri atau share kepada orang lain secara gratis
+*******************************************************************************************************************
+* Dibuat oleh Ikromul Umam https://t.me/arnetadotid
+*******************************************************************************************************************
+* © 2024 Arneta.ID By https://fb.me/umblox
+*******************************************************************************************************************
+*/
+
+// Set the HTTP response code to 403 (Forbidden)
+http_response_code(403);
+
+// Display a forbidden message in plain text
+echo "403 Forbidden - Directory access is forbidden.";
+exit;
